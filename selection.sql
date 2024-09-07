dadf97aeb78a48db262172b0a43e00f894d849de
velugu <garlapativelugondaiah45@gmail.com>
2024-09-07 16:02:21 +0530

    Updated package.json and selection.sql

--- selection.sql
+++ selection.sql
@@ -1,16 +1,9 @@
-CREATE TABLE register(id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT UNIQUE , password TEXT );
+-- CREATE TABLE register(id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT UNIQUE , password TEXT )
 
-INSERT INTO register(id , name , password)
-VALUES
-(2 , 'hanuman' , 'hanuman@2024'),
-(3 , 'vikram' , 'vikram@2024'),
-(4 , 'preethi' , 'preethi@2024'),
-(5 , 'venkatrao' , 'venkatrao@2024'),
-(6 , 'srinu' , 'srinu@2024');
-
-SELECT * FROM register;
+-- SELECT * FROM register;
 
 -- DELETE from register
 -- where id = 2;
 
-DROP TABLE register
+
+-- DROP TABLE register;
